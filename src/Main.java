4471b17d94e1b23f7b8b351fc810a11fab292050
Joseph Searle <josephwilliamsearle@gmail.com>
2023-11-13 10:13:50 +0000

    diagonalDifference problem solution

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>();
-        nums.add(1);
-        nums.add(3);
-        nums.add(3);
-        nums.add(4);
-        nums.add(4);
-        System.out.println(lonelyinteger(nums));
+        List<List<Integer>> nums = new ArrayList<>();
+        List<Integer> n = new ArrayList<>();
+        n.add(1);
+        n.add(2);
+        n.add(3);
+        List<Integer> m = new ArrayList<>();
+        m.add(4);
+        m.add(5);
+        m.add(6);
+        List<Integer> p = new ArrayList<>();
+        p.add(9);
+        p.add(8);
+        p.add(9);
+        nums.add(n);
+        nums.add(m);
+        nums.add(p);
+
+        System.out.println(diagonalDifference(nums));
     }
 
     public static int lonelyinteger(List<Integer> a) {
@@ -25,5 +36,26 @@ public class Main {
             if(set.getValue() == 1) res = set.getKey();
         }
         return res;
+    }
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int h = arr.get(0).size()-1;
+        int sum1 = 0;
+        int sum2 = 0;
+
+        while(i < arr.size()) {
+            sum1 += arr.get(i).get(j);
+            sum2 += arr.get(k).get(h);
+            i += 1;
+            j += 1;
+            k += 1;
+            h -= 1;
+        }
+
+        return Math.abs(sum1 - sum2);
     }
 }
