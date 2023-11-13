4802b75322dcc077ab4f1747758e29f91c4b19d1
Joseph Searle <josephwilliamsearle@gmail.com>
2023-11-13 09:57:49 +0000

    lonelyinterger problem solution

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(3);
+        nums.add(3);
+        nums.add(4);
+        nums.add(4);
+        System.out.println(lonelyinteger(nums));
     }
 
+    public static int lonelyinteger(List<Integer> a) {
+        // Write your code here
+        HashMap<Integer, Integer> numCount = new HashMap<>();
+        int res = 0;
 
+        for(int n : a) {
+           if(numCount.containsKey(n)) numCount.put(n, numCount.get(n)+1);
+           else numCount.put(n, 1);
+        }
+
+        for(Map.Entry<Integer, Integer> set : numCount.entrySet()) {
+            if(set.getValue() == 1) res = set.getKey();
+        }
+        return res;
+    }
 }
